bd4b85996b39b6d384320f04fe10a5b8fe846f57
Your Name <you@example.com>
2025-03-17 17:34:02 +0400

    added gitignore

---
+++ RabbitMQWeb.ExcelCreate/Registrations/AppDbContextRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQWeb.ExcelCreate.Contexts;
+
+namespace RabbitMQWeb.ExcelCreate.Registrations;
+
+public static class AppDbContextRegistration
+{
+    public static void AddAppDbContextRegistration(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseSqlServer(config.GetConnectionString("default"));
+        }).AddIdentity<IdentityUser, IdentityRole>(opt =>
+        {
+            opt.Password.RequireNonAlphanumeric = true;
+            opt.Password.RequireLowercase = true;
+            opt.Password.RequireUppercase = true;
+            opt.Password.RequiredLength = 8;
+            opt.Password.RequireDigit = true;
+            opt.User.RequireUniqueEmail = true;
+            opt.User.AllowedUserNameCharacters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789._";
+        })
+        .AddEntityFrameworkStores<AppDbContext>(); ;
+    }
+}
